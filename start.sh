e6aed518acf4c344d6a72c6289757cbf4ae06354
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-06-25 14:29:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Gowdru123/supraspeedn.git /supraspeedn
+  git clone https://github.com/Gowdru123/supraspeednr.git /supraspeednr
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /supraspeedn
 fi
-cd /supraspeedn
+cd /supraspeednr
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
